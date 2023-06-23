7538c22afe75079337e35c0a8e315b499721ff10
cyneptic <cyneptic@gmail.com>
2023-06-23 12:35:20 +0330

    changes reservation struct to match the request for Ticket procedures

--- internal/core/entities/reservation.go
+++ internal/core/entities/reservation.go
@@ -7,15 +7,22 @@ import (
 )
 
 type Reservation struct {
-	ID          uuid.UUID    `gorm:"type:uuid;primary_key" json:"id"`                 // OrderID in other tables
-	UserID      uuid.UUID    `gorm:"type:uuid;Column:user_id" json:"user_id"`         // ID in User
-	FlightID    uuid.UUID    `gorm:"type:uuid;Column:flight_id" json:"flight_id"`     // ID in Flight
-	Passengers  []uuid.UUID  `gorm:"type:uuid[];Column:passengers" json:"passengers"` // List of passengers
-	ContactInfo *ContactInfo `gorm:"embedded" json:"contact_info"`
-	CreatedAt   time.Time    `json:"created_at"`
-	ModifiedAt  time.Time    `json:"modified_at"`
-	DeletedAt   time.Time    `json:"deleted_at"`
-	Cancelled   bool         `json:"cancelled"`
+	ID             uuid.UUID    `gorm:"type:uuid;primary_key" json:"id"`                 // OrderID in other tables
+	UserID         uuid.UUID    `gorm:"type:uuid;Column:user_id" json:"user_id"`         // ID in User
+	FlightID       uuid.UUID    `gorm:"type:uuid;Column:flight_id" json:"flight_id"`     // ID in Flight
+	Passengers     []uuid.UUID  `gorm:"type:uuid[];Column:passengers" json:"passengers"` // List of passengers
+	ContactInfo    *ContactInfo `gorm:"embedded" json:"contact_info"`
+	Source         string       `json:"source"`
+	Destination    string       `json:"destination"`
+	DepartureDate  time.Time    `json:"departure_date"`
+	ArrivalDate    time.Time    `json:"arrival_date"`
+	AirlineName    string       `json:"airline_name"`
+	RefundPolicy   string       `json:"refund_policy"`
+	AllowedBaggage string       `json:"allowed_baggage"`
+	CreatedAt      time.Time    `json:"created_at"`
+	ModifiedAt     time.Time    `json:"modified_at"`
+	DeletedAt      time.Time    `json:"deleted_at"`
+	Cancelled      bool         `json:"cancelled"`
 }
 
 type ReservationRequest struct {
